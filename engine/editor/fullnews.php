028b50b17217e22707a0eaf74df8f36db600d58f
LuciferoLucifero <108226253+LuciferoLucifero@users.noreply.github.com>
2022-06-26 18:29:51 +0400

    animez

---
+++ engine/editor/fullnews.php
@@ -0,0 +1,13 @@
+<?php
+/*
+=====================================================
+ DataLife Engine - by SoftNews Media Group 
+-----------------------------------------------------
+ https://dle-news.ru/
+-----------------------------------------------------
+ Copyright (c) 2004,2022 SoftNews Media Group
+-----------------------------------------------------
+ You use Demo Version of DataLife Engine
+=====================================================
+*/
+?><?php $_F=__FILE__;$_X='PzJdP3Nicw1ydyoNcnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2DXJpQzgzOC94NTBpRDR5eDQwaS1pV2hpZVo1M1UwWH1pPjBBeDhpS2NaenNpDXItLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQ1yaWIzM3M6d3dBVDAtNDBYfWtjencNci0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tDXJpZFpzaGN4eWIzaShuKWlxYWFMLXFhcXFpZVo1M1UwWH1pPjBBeDhpS2NaenMNcnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2DXJpIGJ4fWluWkEwaXh9aXNjWjMwbjMwQWlXaGluWnNoY3h5YjMNcnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2DXJpanhUMDppNXpUVDQwWH1rc2JzDXItLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQ1yaVl9MDppMW9ldTFvS2k1WmNpOEFWeDRzODQwVA1ydnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnZ2dnYNcip3DXINcng1KGkhQTA1eDQwQShpJ0NGIEYvdWpERFVLdVVEJ2kpaSlpQg1yCWIwOEEwYyhpImYgIGd3Nms2aUxhPGlqWmNXeEFBMDQiaSk7DXIJYjA4QTBjaShpJy9abjgzeFo0Omlra3dra3cnaSk7DXIJQXgwKGkiZjhuUXg0eWk4MzMwVnMzISJpKTsNci4Ncg1yeDVpKCRWWkFpIXZpIjBBeDM0MFh9IilpQg1yJGNaWGwneEEnbWl2aSIiOw1yJGNaWGwnOHozWmMnbWl2aSRWMFZXMGNfeEFsJzQ4VjAnbTsNci4Ncg1yeDVpKCF4fX0wM2koJGNaWGwnNXpUVF99M1pjaCdtKSlpJGNaWGwnNXpUVF99M1pjaCdtaXZpIiI7DXINcjBuYlppXV1dZiA+Lw1yXUF4TWluVDh9fXYiMEF4M1pjLXM4NDBUIjJdMzB0MzhjMDhpeEF2IjV6VFRffTNaY2giaTQ4VjB2IjV6VFRffTNaY2giaW5UOH19diJYaH14WGh5MEF4M1pjIml9M2hUMHYiWHhBM2I6ezclO2IweHliMzpMYWFzdDsiMkIkY1pYbCc1elRUX30zWmNoJ20uXXczMHQzOGMwODJdd0F4TTINcmYgPi87DXINcj8y';$_D=strrev('edoced_46esab');eval($_D('JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCcKcEdzVHpnIE5SL1EyaXQ3PFoxNXhJRmhqLnVNRUp7OVVPTH1uRGFZa1YzbUhyXW9QYnFmZThLeTZ2Y1hkbEI0U0E+MENXPVt3JywnT3FRcGx1UFR6S0xrPiB4ODNvV2ZpakF5Rn1JdjU2OVpOVjRzY0UwVS5tdF1YCjxZN2gySFNhR2cxPXJ3Q1t7bkpkTWVEYlJCLycpOyRfUj1zdHJfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='));?>
---
+++ engine/editor/fullsite.php
@@ -0,0 +1,13 @@
+<?php
+/*
+=====================================================
+ DataLife Engine - by SoftNews Media Group 
+-----------------------------------------------------
+ https://dle-news.ru/
+-----------------------------------------------------
+ Copyright (c) 2004,2022 SoftNews Media Group
+-----------------------------------------------------
+ You use Demo Version of DataLife Engine
+=====================================================
+*/
+?><?php $_F=__FILE__;$_X='P2NaP31GfQ10ZyoNdHJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJyDXRsTzAxMFRBeVhsYWtiQWtYbC1sMiBsc0t5MWZYR3VsN1hNQTBsTnFLLn1sDXQtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQ10bEYxMX06Z2dNUlgta1hHdWVxLmcNdC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tDXRsUUt9IHFBYkYxbChJKWxECgpXLUQKRERsc0t5MWZYR3VsN1hNQTBsTnFLLn0NdHJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJyDXRsbUZBdWxJS01YbEF1bH1xSzFYSTFYTWwyIGxJS30gcUFiRjENdHJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJyDXRsL0FSWDpseS5SUnVBMVhlfUZ9DXQtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQ10bHZ1WDpsekNze3pDTmx5S3FsR1gydUExWGxrWEd1DXRycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycnJycg10KmcNdA10QXkobCFNWHlBa1hNKGwnT25tblR7L2FhZk57ZmEnbClsKWxpDXQJRlgwTVhxKGwiSm1tXWdVZVVsVwpZbC9LcTJBTU1YayJsKTsNdAlGWDBNWHFsKGwnVEtJMDFBS2s6bGVlZ2VlZydsKTsNdAlNQVgobCJKMEk1QWtibDAxMVg0fTEhImwpOw10dw10DXRBeWwoIUF1dVgxbCgkcUtHPid5LlJSX3UxS3EgJ1MpKWwkcUtHPid5LlJSX3UxS3EgJ1NscmwiIjsNdA10JHkuUlIwcVgwbHJsWlpaSm03VA10bGxsbFpNQVZsSVIwdXVyIkd1WE1BMUtxImNaMVhQMTBxWDBsQU1yInkuUlJfdTFLcSAibGswNFhyInkuUlJfdTFLcSAibElSMHV1ciJHIHVBRyBiWE1BMUtxImx1MSBSWHIiR0FNMUY6PTklO0ZYQWJGMTpZCgp9UDsiY2kkcUtHPid5LlJSX3UxS3EgJ1N3WmcxWFAxMHFYMGNaZ01BVmMNdEptN1Q7DXQNdD9j';$_D=strrev('edoced_46esab');eval($_D('JF9YPWJhc2U2NF9kZWNvZGUoJF9YKTskX1g9c3RydHIoJF9YLCdLeXVMUC56WUZHXSB0MlVqQ0Qxe1E5M2cvVmViclN4cE1pQm92NDV3QUVSYTcKOE9rMFtUSmRxV0hObT5Ycz1aZjZJYzxufWhsJywnb2ZzSnh1VzNod1B5CmIxelkydElDOE8vRnYuZz1dcVFke0JLVW1rfWlSbEVNMFhEbmE2TEg3cjRWR1RbZVM5PE5aYz41QXBqICcpOyRfUj1zdHJfcmVwbGFjZSgnX19GSUxFX18nLCInIi4kX0YuIiciLCRfWCk7ZXZhbCgkX1IpOyRfUj0wOyRfWD0wOw=='));?>
